494e065c2a1839cdd448462dc89709c65007d124
pvvng <112927193+pvvng@users.noreply.github.com>
2025-06-26 16:44:35 +0900

    add google search console verification code

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,9 +31,15 @@ export const metadata: Metadata = {
     default: "Lesser UI - 같이 만들고, 같이 나누는 오픈소스 UI",
     template: "%s | Lesser UI",
   },
+
   description:
     "개발자들을 위한 오픈소스 UI 블럭 공유 플랫폼. 손쉽게 UI를 만들고, 영감을 얻고, 함께 나눠보세요.",
+
+  verification: {
+    google: "X8abBNxolxp2vkT2B7w1uCMZm9zkCEGLo0xpbL7Fw9U",
+  },
 };
+
 export default function RootLayout({
   children,
   modal,
